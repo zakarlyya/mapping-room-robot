d0ae79eaa46c1493b0815e247613b0c3843aceb2
MichaelD33 <michael.j.delaney@vanderbilt.edu>
2022-11-27 22:27:44 -0600

    Syntax error

--- logic.py
+++ logic.py
@@ -64,7 +64,7 @@ def logic_main():
     sensor_socket.setsockopt(zmq.SUBSCRIBE, b"")
 
     # create instance of Robot class
-    robot = Robot(pos=current_pos, direction=current_direction, motor_socket=motor_socket)
+    robot = Robot(pos=current_pos, dir=current_direction, motor_socket=motor_socket)
 
     # wait for the start signal on start socket
     while True:
