2e43a1ea511fee38f6ede1de5a9e9394c8f2a7de
Zakariyya Alquran <zakmza123@gmail.com>
2022-12-01 11:31:22 -0600

    commented sensor.py

--- sensor.py
+++ sensor.py
@@ -5,45 +5,52 @@
 # Pan the sensor back and forth using servos (while measuring with sensor)
 # Sends points to data processing thread with the detected points or if turn is necessary
 
+# import necessary libraries
 import zmq
 import time
-import math
 from Ultrasonic import *
 from servo import *
 
 def sensor_main():
+    # create ZMQ context and PUB socket
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
     socket.bind("tcp://*:5556")
 
+    # create an instance of servo and ultrasonic classes
     pwm=Servo()
-    ultrasonic=Ultrasonic()   
+    ultrasonic=Ultrasonic()  
+
+    # point robot straight ahead 
     pwm.setServoPwm('0', 90)
     pwm.setServoPwm('1', 92)
+
+    # set default values including sensor angle and number of readings per angle
     angle = 0
     increasing = True
-
-    num_trials = 2
+    num_trials = 2 
 
     while True:
+        # get the average distance from the sensor using multiple readings to reduce noise
         distance = 0
         for i in range(num_trials):
-            temp = ultrasonic.get_distance()
-            #print(temp)
-            distance = distance + temp/num_trials
+            distance += ultrasonic.get_distance()/num_trials
 
+        # send the average distance measured for some angle
         socket.send_string("{}, {}".format(angle, distance))
-        #time.sleep(1)
         
+        # update the angle iteratively
         if increasing:
             angle = angle + 1.5
         else:
             angle = angle - 1.5
 
+        # update increasing value to pan in other direction
         if angle == 90:
             increasing = False
         elif angle == -90:
             increasing = True
         
+        # set the robot's head pan angle
         pwm.setServoPwm('0', 90 - angle)
         
