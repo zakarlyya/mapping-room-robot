65d2948f00c2e7c0cd845e0bb32c07823ed74158
Zakariyya Alquran <zakmza123@gmail.com>
2022-12-02 17:54:18 -0600

    changed some values

--- logic.py
+++ logic.py
@@ -162,7 +162,7 @@ def logic_main():
             if(not disregard_data):
                 # calculate the absolute position of the measured object using the robots current position,
                 # measured angle, and measured distance and then add the location to the positions list
-                if(angle <= 20 and distance < 40):
+                if(angle <= 20 and distance < 30):
                     point = robot.calculateAbsolutePosition(angle, distance)
                     points.append(point)
                     # logging.info("Abs point: %s" % point)
