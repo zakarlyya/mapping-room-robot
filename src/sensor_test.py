75ab18eaca3c9731d5c6d83c60bd595b96efcd5c
Zakariyya Alquran <zakmza123@gmail.com>
2022-12-07 16:16:03 -0600

    commented sensor_test

--- src/sensor_test.py
+++ src/sensor_test.py
@@ -3,31 +3,32 @@
 # Point will be sent over a ZMQ PUB/SUB socket to a laptop that will graph the points
 # 
 
+# import necessary libraries
 import zmq
-import time
 import threading
 import math
 import random
 from time import sleep
 from sensor import sensor_main
 
+# function to calculate the absolute position of the measured object using the robots 
+# current position, measured angle, and measured distance
 def calculateAbsolutePosition(angle, distance):
-        # calculate the absolute position of the measured object using the robots current position, 
-        # measured angle, and measured distance and then add the location to the positions list
         return [distance * math.sin(math.radians(angle)), distance * math.cos(math.radians(angle))]
 
+# function to generate random readings that simiulate the sensor's
 def randomData():
     distance = round(random.random()*300-150)
     angle = round(random.random()*180 - 90)
     return "{}, {}".format(angle, distance)
 
 if __name__ == '__main__':
-    position = [0,0]
     # create a zmq PUB/SUB communications_socket to communicate with the server
     context = zmq.Context()
     server_socket = context.socket(zmq.PUB)
     server_socket.bind("tcp://*:5558")
 
+    # create and start the sensor thread
     sensor_thread = threading.Thread(target=sensor_main)
     sensor_thread.start()
 
@@ -38,16 +39,16 @@ if __name__ == '__main__':
 
     while True:
         try:
-            sensor_data = sensor_socket.recv_string()
-            #sensor_data = randomData()
-            sensor_data = sensor_data.split(",")    # parse the sensor data as [angle], [distance]
+            #sensor_data = randomData()                 # get random sensor data 
+            sensor_data = sensor_socket.recv_string()   # recieve reading from sensors
+            sensor_data = sensor_data.split(",")        # parse the sensor data as [angle], [distance]
 
-            # calculate the absolute position of the measured object using the robots current position,
-            # measured angle, and measured distance and then add the location to the positions list
+            # calculate the absolute position of the measured object 
             point = calculateAbsolutePosition(float(sensor_data[0]), float(sensor_data[1]))
             print("{}, {},point".format(point[0], point[1]))
             #sleep(0.1)
             server_socket.send_string("{}, {},point".format(point[0], point[1]))
+
         except KeyboardInterrupt:
             print("Closing server socket")
             server_socket.close()
